a81bbae61648025052cd1933d3a4e7af41f00b7f
Joao Barea <jvbarea75@usp.br>
2022-11-30 21:19:29 -0300

    Adding complete System Updating sistemaVentilacao testbench

--- testbenchSV.v
+++ testbenchSV.v
@@ -18,9 +18,80 @@ initial
 begin
     presRea = -1;
     presTubSR = -2;
+    $write("DumberSRSR: %2d Ativado? ", damperRSRA);
+    if (damperRSRA==1) $display ("Sim");
+    else if (damperRSRA==0) $display("Nao");
+    else $display("Not sure");
+    presRea = -2;
+    presTubSR = -1;
+    $write("DumberSRSR: %2d Ativado? ", damperRSRA);
+    if (damperRSRA==1) $display ("Sim");
+    else if (damperRSRA==0) $display("Nao");
+    else $display("Not sure");
+
+    presTubSR = -1;
+    presS3 = -2;
+    $write("DumberS3SR: %2d Ativado? ", damperS3SRA);
+    if ( damperS3SRA==1) $display ("Sim");
+    else if ( damperS3SRA==0) $display("Nao");
+    else $display("Not sure");
+    presTubSR = -2;
+    presS3 = -1;
+    $write("DumberS3SR: %2d Ativado? ", damperS3SRA);
+    if ( damperS3SRA==1) $display ("Sim");
+    else if ( damperS3SRA==0) $display("Nao");
+    else $display("Not sure");
+
+    presS3 = -1;
+    presS2 = -2;
+    $write("DumberS23: %2d Ativado? ", damperS23A);
+    if ( damperS23A==1) $display ("Sim");
+    else if ( damperS23A==0) $display("Nao");
+    else $display("Not sure");
+    presS3 = -2;
+    presS2 = -1;
+    $write("DumberS23: %2d Ativado? ", damperS23A);
+    if ( damperS23A==1) $display ("Sim");
+    else if ( damperS23A==0) $display("Nao");
+    else $display("Not sure");
+
+    presS2 = -1;
+    presS1 = -2;
     $write("DumberS12: %2d Ativado? ", damperS12A);
-    if (damperS12A==1) $display ("Sim");
-    else if (damperS12A==0) $display("Nao");
+    if ( damperS12A==1) $display ("Sim");
+    else if ( damperS12A==0) $display("Nao");
+    else $display("Not sure");
+    presS2 = -2;
+    presS1 = -1;
+    $write("DumberS12: %2d Ativado? ", damperS12A);
+    if ( damperS12A==1) $display ("Sim");
+    else if ( damperS12A==0) $display("Nao");
+    else $display("Not sure");
+
+    presS2 = -1;
+    presS1 = -2;
+    $write("DumberS3SS: %2d Ativado? ", damperS3SSA);
+    if (damperS3SSA==1) $display ("Sim");
+    else if (damperS3SSA==0) $display("Nao");
+    else $display("Not sure");
+    presS2 = -2;
+    presS1 = -1;
+    $write("DumberS3SS: %2d Ativado? ", damperS3SSA);
+    if (damperS3SSA==1) $display ("Sim");
+    else if (damperS3SSA==0) $display("Nao");
+    else $display("Not sure");
+
+    presS2 = -1;
+    presS1 = -2;
+    $write("DumberSSSC: %2d Ativado? ", damperSSSCA);
+    if (damperSSSCA==1) $display ("Sim");
+    else if (damperSSSCA==0) $display("Nao");
+    else $display("Not sure");
+    presS2 = -2;
+    presS1 = -1;
+    $write("DumberSSSC: %2d Ativado? ", damperSSSCA);
+    if (damperSSSCA==1) $display ("Sim");
+    else if (damperSSSCA==0) $display("Nao");
     else $display("Not sure");
 end
 endmodule
